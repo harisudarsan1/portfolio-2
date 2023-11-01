353556d2460338b026a9d361bb66bfce33dd0b89
Harisudarsan <sudarshanhari561@gmail.com>
2023-11-01 19:59:37 +0530

    added analytics components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
   title: "Harisudarsan",
   description: "personal portfolio website of harisudarsan",
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={` bg-bg3 text-black-900 ${inter.className} `}>
         <Navbar></Navbar>
         {children}
+        <Analytics />
       </body>
     </html>
   );
